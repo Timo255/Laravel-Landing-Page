10b81600e5dc22a07bd8af201a9042b6a2dfd4ca
Timothy <104308259+Timo255@users.noreply.github.com>
2025-10-01 07:33:23 +0300

    Update product-livewire.blade.php

--- resources/views/livewire/product-livewire.blade.php
+++ resources/views/livewire/product-livewire.blade.php
@@ -1,63 +1,65 @@
-<div>
-    <div class="wrapper">
-        <div id="dI" class="Detail-imd">
-            <div class="productDetails">
-                <h1 class="product-header">Smart Digital Watch With 30% Off</h1>
-                <p class="disc">Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Voluptates rerum autem nesciunt aliquam iure, tempore exercitationem repellendus
-                    dolorem error reiciendis ut cum optio illum doloribus culpa.</p>
-                <button class="topOffer" id="topOffer-btn" wire:click="openPopUp">Claim Deal
-                    Now</button>
-            </div>
-            <div class="product-image">
-                <div class="product-image">
-                    <img srcset="
-                        {{ $BigWatch }} 333w, 
-                        {{ $mediumWatch }} 254w, 
-                        {{ $smallWatch }} 206w"
-                        sizes="(min-width: 1200px) 333px, 
-                               (min-width: 768px) 254px, 
-                               (min-width: 360px) 206px"
-                        src="{{ asset($BigWatch) }}" />
+<div class="wrapper">
+    <div id="dI" class="Detail-imd">
+        <!-- Product details -->
+        <div class="productDetails">
+            <h1 class="product-header">Smart Digital Watch With 30% Off</h1>
+            <p class="disc">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Voluptates rerum autem nesciunt aliquam iure, tempore exercitationem repellendus
+                dolorem error reiciendis ut cum optio illum doloribus culpa.
+            </p>
+            <button class="topOffer" wire:click="openPopUp">
+                Claim Deal Now
+            </button>
+        </div>
+
+        <!-- Product image -->
+        <div class="product-image">
+            <img 
+                srcset="
+                    {{ $BigWatch }} 333w, 
+                    {{ $mediumWatch }} 254w, 
+                    {{ $smallWatch }} 206w"
+                sizes="(min-width: 1200px) 333px, 
+                       (min-width: 768px) 254px, 
+                       (min-width: 360px) 206px"
+                src="{{ asset($BigWatch) }}" 
+                alt="Smart Watch"
+            />
+        </div>
+    </div>
+
+    <!-- Popup modal -->
+    <div class="popup {{ $openPopUp ? 'active' : '' }}">
+        <!-- Close button -->
+        <div class="popup-close-btn" wire:click="closePopUp">
+            <img src="{{ asset('images/close-btn1.png') }}" alt="Close" />
+        </div>
+
+        <h1 class="popH1">Subscribe and check your Email</h1>
+
+        <!-- Subscribe form -->
+        <form class="formFields" wire:submit.prevent="subscribe" method="post">
+            @csrf
+
+            @if ($errors->any())
+                <div>
+                    @foreach ($errors->all() as $error)
+                        <div style="text-align: center; color: red;">{{ $error }}</div>
+                    @endforeach
                 </div>
+            @endif
 
-            </div>
-        </div>
+            <input type="text" placeholder="Enter First Name *" wire:model="first_name" required />
+            <input type="text" placeholder="Enter Last Name *" wire:model="last_name" required />
+            <input type="email" placeholder="Enter Your Email *" wire:model="email" required />
 
-        <!-- Popup section -->
-        <div class='popup {{ $openPopUp ? 'active' : '' }}'>
-            <div class="popup-close-btn" wire:click="closePopUp">
-                <img src="images/close-btn1.png" alt="" />
-            </div>
-            <h1 class="popH1">Subscribe and check your Email</h1>
-            {{-- onSubmit={handleSubmit}  --}}
-            <form class="formFields" wire:submit.prevent="subscribe" method="post">
-                @csrf
-                @if ($errors->any())
-                    <div>
-                        @foreach ($errors->all() as $error)
-                            <div style="text-align: center; color: red;">{{ $error }}</div>
-                        @endforeach
-                    </div>
-                @endif
-                {{-- {firstNameError && <InlineError error={firstNameError}/>} --}}
-                <input type="text" placeholder="Enter First Name *" wire:model="first_name" {{-- onChange={(e)=>setFirstName(e.target.value)} --}}
-                    required />
-                {{-- {lastNameError && <InlineError error={lastNameError}/>} --}}
-                <input type="text" placeholder="Enter Last Name *" wire:model="last_name" {{-- onChange={(e)=>setLastName(e.target.value)} --}}
-                    required />
-                {{-- {emailError && <InlineError error={emailError}/>} --}}
-                <input type="email" placeholder="Enter Your Email *" wire:model="email" {{-- onChange={(e)=>setEmail(e.target.value)} --}}
-                    required />
-                {{-- {statusM.msg ? <p style={{color: `${statusM.success === false ? "red" : "green"}`, marginBottom: "10px"}}>{statusM.msg}</p> : <></>} --}}
-                <p style="color: green">{{ $success }}</p>
+            <p style="color: green">{{ $success }}</p>
 
-                <button type="submit" wire:loading.attr="disabled">
-                    <span wire:loading.remove>Subscribe</span>
-                    <span wire:loading>Subscribing...</span>
-                </button>
-
-            </form>
-        </div>
+            <button type="submit" wire:loading.attr="disabled">
+                <span wire:loading.remove>Subscribe</span>
+                <span wire:loading>Subscribing...</span>
+            </button>
+        </form>
     </div>
 </div>
